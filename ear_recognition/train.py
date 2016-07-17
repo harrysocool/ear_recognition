e5644eddbe298feab09a2da104898d698d438988
harrysocool <harry_so_cool@live.com>
2016-07-17 23:26:12 +0100

    .

---
+++ ear_recognition/train.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 16/7/1 15:20 2016
+
+@author: harry sun
+"""
+
+./tools/train_net.py --solver models/VGG_CNN_M_1024/solver.prototxt --weights data/imagenet_models/VGG_CNN_M_1024.v2.caffemodel --imdb soton_ear

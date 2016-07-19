7412571fe2bf951638c06869d0a3bb44f44e65a1
harrysocool <harry_so_cool@live.com>
2016-07-19 21:16:33 +0100

    .

--- ear_recognition/draft.m
+++ ear_recognition/draft.m
@@ -1,3 +1,3 @@
-addpath('OP_mathods/edges/')
+addpath(genpath('/Users/harrysocool/Github/fast-rcnn/OP_methods/edges/'))
 % selective_search({'/Users/harrysocool/Github/fast-rcnn/2.jpg'}, 'mat_file/1.mat');
-edge_detector({'/Users/harrysocool/Github/fast-rcnn/2.jpg'}, './ear_recognition/mat_file/1.mat');
+edge_detector({'/Users/harrysocool/Github/fast-rcnn/2.jpg'}, '/Users/harrysocool/Github/fast-rcnn/ear_recognition/data_file/1.mat');
